0c642bc6250116dcc790ce146bd27186e0520a47
Mateus Santos Fonseca <mateus.santos.fsc@gmail.com>
2020-08-14 00:12:31 -0300

    Updates

---
+++ precache-manifest.1f4b593dc03bb97b4f144db38fa430d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98ddd3775d2cc340f9c473302e5eafea",
+    "url": "/devils-scheduler/index.html"
+  },
+  {
+    "revision": "64d5fe7861b34e21e6fa",
+    "url": "/devils-scheduler/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "d434fef7284f6f21c374",
+    "url": "/devils-scheduler/static/css/main.3d9ed659.chunk.css"
+  },
+  {
+    "revision": "64d5fe7861b34e21e6fa",
+    "url": "/devils-scheduler/static/js/2.15fe0298.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/devils-scheduler/static/js/2.15fe0298.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d434fef7284f6f21c374",
+    "url": "/devils-scheduler/static/js/main.c11ec47d.chunk.js"
+  },
+  {
+    "revision": "84c78f8b69e527873a65",
+    "url": "/devils-scheduler/static/js/runtime-main.36460e2d.js"
+  }
+]);
