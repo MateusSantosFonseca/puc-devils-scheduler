24445cebb5c0335f3a267fb091082e92ed6b76e0
Mateus Santos Fonseca <mateus.santos.fsc@gmail.com>
2020-08-16 10:32:15 -0300

    Removendo comentários e refatorando código da API

--- Backend/api/login.js
+++ Backend/api/login.js
@@ -5,19 +5,12 @@ var database = getFirebase()
 
 const checkLogin = (req, res) => {
   const credenciais = req.body;
-  console.log(credenciais)
   return database.ref('/logins/' + credenciais.login).once('value').then(function (snapshot) {
-    var usuarioEncontrado = snapshot.exists() ? true : false;
-    if (!usuarioEncontrado){
-      console.log("errado")
+    if (!snapshot.exists()) return res.json({ result: false });
+    if (snapshot.val() !== credenciais.senha)
       return res.json({ result: false });
-    } 
-    if (snapshot.val() !== credenciais.senha) {
-      console.log("errado")
-      return res.json({ result: false });
-    } else {
-      console.log("certo")
-      return res.json({ result: usuarioEncontrado });
+    else {
+      return res.json({ result: true });
     }
   })
 }
