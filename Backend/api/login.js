c6dfb7e88a1283b8a8c253406882c53714a660fe
Mateus Fonseca <mateus.santos.fsc@gmail.com>
2020-11-18 23:01:29 -0300

    Alteração de nome de argumento no backend

--- Backend/api/login.js
+++ Backend/api/login.js
@@ -5,7 +5,7 @@ var database = getFirebase()
 
 const checkLogin = (req, res) => {
   const credenciais = req.body;
-  return database.ref('/logins/' + credenciais.login).once('value').then((snapshot) => {
+  return database.ref('/logins/' + credenciais.usuario).once('value').then((snapshot) => {
     if (!snapshot.exists()) return res.json({ result: false });
     if (snapshot.val() !== credenciais.senha)
       return res.json({ result: false });
